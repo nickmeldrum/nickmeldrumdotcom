9d9cf75c44263c39e2d96feddb9aa0b9bb7c2c0d
Nick Meldrum <nick@nickmeldrum.com>
2018-03-28 16:33:33 +0100

    brokwn test wip

--- lambda/indexhtml-rewrite.test.js
+++ lambda/indexhtml-rewrite.test.js
@@ -22,17 +22,26 @@ describe('rewrite handler', () => {
       uri: 'http://www.something.com/somewhere/over/the/rainbow///',
       expected: 'http://www.something.com/somewhere/over/the/rainbow',
     },
-  ].forEach(testData => {
-    it('redirects on a trailing slash', () => {
-      testUri(testData.uri).andAssert(request =>
-        expect(request.status).toEqual('301')
-      )
+  ]
+    .forEach(testData => {
+      it('redirects on a trailing slash', () => {
+        testUri(testData.uri).andAssert(request =>
+          expect(request.status).toEqual('301')
+        )
+      })
+
+      it('redirects to a location without trailing slashes', () => {
+        testUri(testData.uri).andAssert(request =>
+          expect(request.headers.location[0].value).toEqual(testData.expected)
+        )
+      })
     })
 
-    it('redirects to a location without trailing slashes', () => {
-      testUri(testData.uri).andAssert(request =>
-        expect(request.headers.location[0].value).toEqual(testData.expected)
-      )
+    [{ uri: 'something' }].forEach(testData => {
+      it('does not redirect', () => {
+        testUri(testData.uri).andAssert(request =>
+          expect(request.status).toEqual('200')
+        )
+      })
     })
-  })
 })
