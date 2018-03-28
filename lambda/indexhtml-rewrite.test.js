e07ba593bb69063f1a3dde3b91ea6311be8395e0
Nick Meldrum <nick@nickmeldrum.com>
2018-03-28 16:29:31 +0100

    improve jest mocking/stubbing

--- lambda/indexhtml-rewrite.test.js
+++ lambda/indexhtml-rewrite.test.js
@@ -2,22 +2,37 @@ const { handler } = require('./indexhtml-rewrite')
 
 const testUri = uri => ({
   andAssert: assertion => {
-    let callbackCalled = false
-
-    const event = { Records: [{ cf: { request: { uri: uri } } }] }
-    handler(event, {}, (something, request) => {
+    const mockCallback = jest.fn().mockImplementation((something, request) => {
       assertion(request)
-      callbackCalled = true
     })
 
-    expect(callbackCalled).toEqual(true)
+    const event = { Records: [{ cf: { request: { uri: uri } } }] }
+    handler(event, {}, mockCallback)
+
+    expect(mockCallback).toHaveBeenCalled()
   },
 })
 
 describe('rewrite handler', () => {
-  it('redirects on a trailing slash', () => {
-    testUri('something/').andAssert(request =>
-      expect(request.status).toEqual('301')
-    )
+  [
+    { uri: 'something/', expected: 'something' },
+    { uri: 'something//', expected: 'something' },
+    { uri: 'something///', expected: 'something' },
+    {
+      uri: 'http://www.something.com/somewhere/over/the/rainbow///',
+      expected: 'http://www.something.com/somewhere/over/the/rainbow',
+    },
+  ].forEach(testData => {
+    it('redirects on a trailing slash', () => {
+      testUri(testData.uri).andAssert(request =>
+        expect(request.status).toEqual('301')
+      )
+    })
+
+    it('redirects to a location without trailing slashes', () => {
+      testUri(testData.uri).andAssert(request =>
+        expect(request.headers.location[0].value).toEqual(testData.expected)
+      )
+    })
   })
 })
