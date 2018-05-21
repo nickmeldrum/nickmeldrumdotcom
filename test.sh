eccf38db144deb185c966eac62aac75a1c15a5d4
Nick Meldrum <nick@nickmeldrum.com>
2018-05-21 12:56:16 +0100

    fix bug in create stack creation and fixed OAI link in template

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-nickmeldrumcomblog_testing_infrastructure_stack_id=$(aws cloudformation describe-stacks --stack-name nickmeldrumcomblog-testing-infrastructure --output text --query Stacks[*].StackId --region us-east-1)
-echo $nickmeldrumcomblog_testing_infrastructure_stack_id
-#aws cloudformation describe-stacks --stack-name nickmeldrumcomblog-testing-infrastructure --output text --query Stacks[*].StackId --region us-east-1
