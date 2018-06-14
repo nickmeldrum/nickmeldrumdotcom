60645af5f9524d8ab7fc2849b5e0dcc3448db509
Nick Meldrum <nick@nickmeldrum.com>
2018-06-14 15:00:39 +0100

    stop hiding exit code

--- infrastructure/index.mjs
+++ infrastructure/index.mjs
@@ -4,13 +4,9 @@ import updateContent from './src/s3'
 import createOrUpdateLambda from './src/lambda'
 
 const executeasync = async () => {
-  try {
-    await setup()
-    await createOrUpdateStack()
-    await updateContent()
-    await createOrUpdateLambda()
-  } catch (e) {
-    console.error('unhandled error, exiting', e)
-  }
+  await setup()
+  await createOrUpdateStack()
+  await updateContent()
+  await createOrUpdateLambda()
 }
 executeasync()
